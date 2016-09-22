bc8d845b48a31d12e2422b26986c2fdfa074a4b2
Simeon de Dios <simeon.dedios@gmail.com>
2016-09-22 14:10:38 -0400

    Added support for new cache.

--- examples/query.js
+++ examples/query.js
@@ -2,7 +2,8 @@
 
 const
     _ = require('lodash'),
-    geohash = require('../index');
+    geohash = require('../index'),
+    schools = require('./schools');
 
 module.exports = function(dynamoDB) {
     return querySchools(dynamoDB);
@@ -11,14 +12,29 @@ module.exports = function(dynamoDB) {
 
 function querySchools(dynamoDB) {
 
+    // FIXME: simulate cache for now
+    const geohashCache = new Set();
+
     // create client
-    const geohashClient = new geohash.DynamoGeospatialClient({ dynamoDB: dynamoDB});
+    const geohashClient = new geohash.DynamoGeospatialClient({
+        dynamoDB: dynamoDB,
+        cache: geohashCache
+    });
+
+    // populate cache
+    _.forEach(schools, (school) => {
+        geohashCache.add(geohashClient.getHashKey(school.Location.lat, school.Location.lng));
+    });
+    console.log('cache size: ' + geohashCache.size);
 
     // add some schools
     return geohashClient
         .queryAsync('Schools',
-            43.878940675173695, -79.32474502978516,
-            44.02723709958617, -79.05008682666016)
-        .then((schools) => _.forEach(schools, (school) => console.log('found school: %s', school.name)));
+            43.60311967129431, -79.72756247167979,
+            43.752101797181005, -79.45290426855479)
+        .then((schools) => {
+            console.log('found %d schools', schools.length);
+            _.forEach(schools, (school) => console.log('   %s', school.name))
+        });
 }
 
